f310e4dc727507bf79724c33e8f14b6f8fd22fc0
Alex Po <alexp@eastvisionsystems.com>
2015-05-19 18:14:25 +0300

    [feature] added blog post 2015-05-19:18:14:25

--- publish.sh
+++ publish.sh
@@ -4,8 +4,12 @@ cd /var/www/faculty.dev/blog-tools.dev/blog.dev
 # add all added/modified files
 git add --all .
 # commit changes
-git commit -am "[feature] added blog post `date`"
-git pull origin master
-# push to git remote repository
-git push origin master
+git commit -am "[feature] added blog post `date +%Y-%m-%d:%H:%M:%S`"
+
+git checkout gh-pages
+git pull origin gh-pages
+git push origin gh-pages
+
+# git pull origin master
+# git push origin master
 ###########################
